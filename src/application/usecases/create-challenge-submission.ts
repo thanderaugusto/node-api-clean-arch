740b509819d10cd7ea5d982ce47a56ac67cd4bef
Thander Augusto <thander-99@hotmail.com>
2023-06-03 15:22:26 -0300

    init use case challenge submission

---
+++ src/application/usecases/create-challenge-submission.ts
@@ -0,0 +1,18 @@
+import { Submission } from "../../domain/entities/submission";
+
+type CreateChallengeSubmissionRequest = {
+    studentId: string;
+    challengeId: string;
+
+}
+
+export class CreateChallengeSubmission {
+    async execute({ studentId, challengeId }: CreateChallengeSubmissionRequest) {
+        const submission = Submission.create({
+            studentId,
+            challengeId,
+        })
+
+        return submission
+    }
+}
