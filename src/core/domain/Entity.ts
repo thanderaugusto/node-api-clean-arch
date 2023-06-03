afdf07ad4c2bb23ff20b9a65d1a18904d07fa9de
Thander Augusto <thander-99@hotmail.com>
2023-06-03 16:36:22 -0300

    add get and set id in abstract Entity

--- src/core/domain/Entity.ts
+++ src/core/domain/Entity.ts
@@ -1,8 +1,16 @@
-import crypto from 'node:crypto';
+import crypto from 'crypto';
 
 export abstract class Entity<T> {
-    protected _id: string
+    protected _id: string;
     public props: T;
+
+    get id() {
+        return this._id;
+    }
+
+    set id(id) {
+        this._id = id
+    }
 
     constructor(props: T, id?: string) {
         this.props = props;
